d7b3996de3928eb3c7b6273a45e56119c5451952
PavithraNS <nspavi1991@gmail.com>
2023-04-06 08:24:43 +0530

    [Pavithra] create conflict

--- HelloWorldDemo276Batch/Program.cs
+++ HelloWorldDemo276Batch/Program.cs
@@ -12,6 +12,7 @@ namespace HelloWorldDemo276Batch
         {
             Console.WriteLine("Welcome to 276Batch");
             Console.WriteLine("Code pushing from 2nd Develeoper");
+            Console.WriteLine("Modifying code from remote repository");
             Console.ReadLine();
         }
     }
